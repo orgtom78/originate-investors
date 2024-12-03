ff79f9342b77d03e38803f1476d2b2277fbeccb0
orgtom78 <admin@originate.capital>
2024-12-03 23:52:13 +0100

    v0.1

--- src/data/mutations/createuser.ts
+++ src/data/mutations/createuser.ts
@@ -4,8 +4,11 @@ import { type Schema } from '../../../amplify/data/resource'
 const client = generateClient<Schema>();
 
 async function createuser(input: string){ 
-const { errors, data: newUsername } = await client.models.Todo.create({
-  username: input,
+const { errors, data: newUsername } = await client.models.User.create({
+  firstname: input,
+  lastname: input,
+  email: input,
+  phone: input
 })
 }
 
